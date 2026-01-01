ef5a6050bedb772ba5a972cc1d310dfc625aeaa0
Ajumoke-1siodu <georgiaisiodu@gmail.com>
2026-01-01 10:03:45 -0800

    add

--- task_4.sql
+++ task_4.sql
@@ -2,9 +2,8 @@
 -- Switch to the correct database
 USE alx_book_store;
 
--- Select the full description of the 'books' table
+-- Select the full description of the 'Books' table
 SELECT COLUMN_NAME, COLUMN_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
-
+  AND TABLE_NAME = 'Books';

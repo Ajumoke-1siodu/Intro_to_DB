b27c66c604fb54f79e15a8193cd597a10eed55be
Ajumoke-1siodu <georgiaisiodu@gmail.com>
2026-01-01 09:54:06 -0800

    add changes

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,9 @@
--- task_4.sql
 -- Switch to the correct database
 USE alx_book_store;
 
--- Print the full description of the table 'books'
-SHOW CREATE TABLE books;
+-- Query the information schema to get the full description of the 'books' table
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
 
